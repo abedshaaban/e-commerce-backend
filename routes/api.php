5ea3afbb4dfc6ae2b8da592695f539d03089ac25
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-09 20:36:45 +0200

    [clean-up]: removed commented code

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::post("/auth/register", [UserController::class, "register"]);
-// Route::post("/auth/sign-in", [UserController::class, "sign_in"]);
-
-
 Route::controller(AuthController::class)->group(function () {
     Route::post('/auth/login', 'login');
     Route::post('/auth/register', 'register');
@@ -17,17 +13,4 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('/auth/refresh', 'refresh');
 
 });
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
