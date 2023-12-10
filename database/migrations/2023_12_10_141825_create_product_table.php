e9b182e5cb306a2bcd574cd3f6b3a59fb6010bc1
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-10 16:24:33 +0200

    [db]: created products table

---
+++ database/migrations/2023_12_10_141825_create_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->float('price');
+            $table->float('quantity');
+            $table
+            ->foreignUuid('user_id')
+            ->references('uuid')
+            ->on('users')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+    }
+};
