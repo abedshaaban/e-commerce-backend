8d6ee1735af1897f8b6a36f8fd78f9ea1f6929c6
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-10 12:47:04 +0200

    [core]: return user data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Utilities\Helpers;
 
 
 class UserController extends Controller
 {
     public function get_user_info(Request $request)
     {
-        return  response()->json(auth()->payload());
+        $user = [];
+        $token_data = auth()->payload();
+
+        $user['uuid'] = $token_data->get('uuid');
+        $user['email'] = $token_data->get('email');
+        $user['f_name'] = $token_data->get('f_name');
+        $user['l_name'] = $token_data->get('l-name');
+        $user['role'] = $token_data->get('role');
+
+        return  response()->json($user);
     }
 }
